1d2b7773ba34a390e2b8abde0a33cd97a76f0e73
richardesp <espantaleon2001@gmail.com>
2024-12-24 10:48:59 +0100

    feat: initial upload

---
+++ cbg_process/common_code.cpp
@@ -0,0 +1,79 @@
+#include "common_code.hpp"
+
+cv::Mat
+fsiv_convert_image_byte_to_float(const cv::Mat &img)
+{
+    CV_Assert(img.depth() == CV_8U);
+    cv::Mat out;
+    //! TODO
+    // Hint: use cv::Mat::convertTo().
+
+    //
+    CV_Assert(out.rows == img.rows && out.cols == img.cols);
+    CV_Assert(out.depth() == CV_32F);
+    CV_Assert(img.channels() == out.channels());
+    return out;
+}
+
+cv::Mat
+fsiv_convert_image_float_to_byte(const cv::Mat &img)
+{
+    CV_Assert(img.depth() == CV_32F);
+    cv::Mat out;
+    //! TODO
+    // Hint: use cv::Mat::convertTo()
+
+    //
+    CV_Assert(out.rows == img.rows && out.cols == img.cols);
+    CV_Assert(out.depth() == CV_8U);
+    CV_Assert(img.channels() == out.channels());
+    return out;
+}
+
+cv::Mat
+fsiv_convert_bgr_to_hsv(const cv::Mat &img)
+{
+    CV_Assert(img.channels() == 3);
+    cv::Mat out;
+    //! TODO
+    // Hint: use cvtColor.
+    // Remember: the input color scheme is assumed to be BGR.
+
+    //
+    CV_Assert(out.channels() == 3);
+    return out;
+}
+
+cv::Mat
+fsiv_convert_hsv_to_bgr(const cv::Mat &img)
+{
+    CV_Assert(img.channels() == 3);
+    cv::Mat out;
+    //! TODO
+    // Hint: use cvtColor.
+    // Remember: the ouput color scheme is assumed to be BGR.
+
+    //
+    CV_Assert(out.channels() == 3);
+    return out;
+}
+
+cv::Mat
+fsiv_cbg_process(const cv::Mat &in,
+                 double contrast, double brightness, double gamma,
+                 bool only_luma)
+{
+    CV_Assert(in.depth() == CV_8U);
+    cv::Mat out;
+    // TODO
+    // Hint: convert to float range [0,1] before processing the image.
+    // Hint: use cv::pow() to apply the gamma parameter.
+    // Hint: if input channels is 3 and only luma is required, convert to HSV
+    //       color space and process only de V (luma) channel.
+
+    //
+    CV_Assert(out.rows == in.rows && out.cols == in.cols);
+    CV_Assert(out.depth() == CV_8U);
+    CV_Assert(out.channels() == in.channels());
+    return out;
+}
